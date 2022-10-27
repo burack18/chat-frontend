a8499f8b0b7c48bcfe6e5b33f0d6bacef0f828de
burack <omerfaruk.aksak@student.ucll.be>
2022-10-27 19:45:06 +0200

    added primarysearchbarr

--- src/shared/layout/components/Layout.js
+++ src/shared/layout/components/Layout.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import PrimarySearchAppBar from './PrimarySearchAppBar';
 import StickyFooter from './StickyFooter';
 
 export default function Layout() {
-  return(
-    <div>Layout
-        <Outlet/>
-
-        <StickyFooter/>
+  return (
+    <div>
+      <PrimarySearchAppBar />
+      <Outlet />
+      <StickyFooter />
     </div>
   );
 }
